6d764690d72c11bd423f7e927d45d060232c222b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-02-10 07:43:53 -0700

    Updated elements from branch

--- src/main/java/com/evolutionnext/order/domain/aggregate/OrderEvent.java
+++ src/main/java/com/evolutionnext/order/domain/aggregate/OrderEvent.java
@@ -1,6 +1,6 @@
 package com.evolutionnext.order.domain.aggregate;
 
 
-public sealed interface OrderEvent permits OrderPlaced, OrderCancelled, OrderCreated {
+public sealed interface OrderEvent permits OrderCancelled, OrderCreated, OrderPlaced {
 
 }
--- src/main/java/com/evolutionnext/order/domain/application/OrderApplicationService.java
+++ src/main/java/com/evolutionnext/order/domain/application/OrderApplicationService.java
@@ -14,5 +14,6 @@ public class OrderApplicationService {
 
     public void submit(Order order) {
         order.events().forEach(orderEventPublisher::publish);
+        order.clearEvents();
     }
 }
